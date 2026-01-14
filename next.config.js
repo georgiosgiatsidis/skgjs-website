2f5a16a4faacd813039df342f642386ef793156f
Georgios Giatsidis <georgiosgiatsidis@hotmail.gr>
2026-01-14 21:29:24 +0200

    add environment variables for TEBI configuration

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '' : '',
   trailingSlash: true,
+  env: {
+    TEBI_ACCESS_KEY_ID: process.env.TEBI_ACCESS_KEY_ID,
+    TEBI_SECRET_ACCESS_KEY: process.env.TEBI_SECRET_ACCESS_KEY,
+    TEBI_BUCKET_NAME: process.env.TEBI_BUCKET_NAME,
+  },
 }
 
 module.exports = nextConfig
